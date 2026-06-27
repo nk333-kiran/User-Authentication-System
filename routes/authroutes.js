78fc375f9c0689f0b8b6b2bebe91a4b7120d3dd9
Navaneeth <mnavaneeth321@gmail.com>
2026-06-27 18:37:07 +0530

    Create authroutes.js

---
+++ routes/authroutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+
+router.get("/profile", authMiddleware, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user
+  });
+});
+
+module.exports = router;
